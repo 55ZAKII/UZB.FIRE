329241f99c962afe3d794080104546d903fc1a3d
55zakii <zodilbek2009@gmail.com>
2023-11-23 19:59:01 +0500

    wee commit

---
+++ style.js
@@ -0,0 +1,61 @@
+// let narx = 10000;
+
+// if (narx < 10000) {
+//   console.log('nok');
+// } else if (narx >= 10000 && narx < 12000) {
+//   console.log('olma');
+// } else if (narx >= 12000 && narx < 20000) {
+//   console.log('tarvuz');
+// } else if (narx >= 20000) {
+//   console.log('pomidor');
+// } else {
+//   console.log('unday meva yoq');
+// }
+
+//////////////////////////////////////////////////////////////
+
+// var ball = 100;
+// if (ball >= 86 && ball <= 100) {
+//   console.log("a");
+// } else if (ball >= 61 && ball <= 85) {
+//   console.log("b");
+// } else if (ball >= 50 && ball <= 60) {
+//   console.log("c");
+// } else {
+//   console.log("faild");
+// }
+
+//////////////////////////////////////////////////////////////
+
+// function onefunction(a, b) {
+//     return a * b;
+//   }
+
+//   console.log(onefunction(500, 2));
+
+//////////////////////////////////////////////////////////////
+
+// function twofunction( a, b, c,) {
+//     return `${a}  ${b}  ${c}`
+// }
+
+// const a = [1234];
+// const b = [5678];
+// const c = [90];
+
+// console.log(twofunction(a,b,c));
+
+//////////////////////////////////////////////////////////////
+
+// function threefunction(a) {
+//     return a
+// }
+
+// var a = '1234567890qwertyuiopASDFGHJKLZXCVBrtyu2345678'
+
+// a = a.match(/[A-z]/g)
+
+// console.log(threefunction(a));
+
+//////////////////////////////////////////////////////////////
+
