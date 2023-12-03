d9a1df696211a34562f669724225ade39b8e3fc7
55zakii <zodilbek2009@gmail.com>
2023-12-03 07:49:43 +0500

    doston commit

--- style.js
+++ style.js
@@ -81,7 +81,7 @@
 //     const reversed = parseInt(i.toString().split('').reverse().join(''));
 //     return Number(reversed + '' + i);
 //   }
-  
+
 //   console.log(reverseAndNot(222)); // Output: 321123
 //   console.log(reverseAndNot(152)); // Output: 251152
 //   console.log(reverseAndNot(123456789)); // Output: 987654321123456789
@@ -106,16 +106,179 @@
 /////////////////////////////////////////////////////////////////
 
 // const name = '@dilbek Z@kirj@n@v';
-// const res = name.match(/[k]/g);
+// const res = name.match(/[@]/g);
 // console.log(res);
 
+// ////////////////////////////////////////////////
 
-////////////////////////////////////////////////
+// function findOccurrences(arr, symbol) {
+//   let occurrences = [];
+//   for (let item of arr) {
+//     let chars = Array.from(item);
+//     for (let char of chars) {
+//       if (char === symbol) {
+//         occurrences.push(char);
+//       }
+//     }
+//   }
+//   return occurrences;
+// }
+// const names = ["x@s@n @bdur@z@kov nnnn"];
+// const symbol = "@";
+// const occurrences = findOccurrences(names, symbol);
+// console.log(occurrences);
 
-const search =['xasan', 'xusan', 'zubayir', 545454354,]
+// ///////////////////////////////////////
 
-const searchFun = () => {
+// const meva = ["olma", "nok", "behi", "banan"];
+// const filteredMeva = meva.filter((fruit) => fruit !== "olma");
+// filteredMeva.forEach((fruit) => {
+//   console.log(fruit);
+// });
+
+// ////////////////////////////////////////////////////////////
+
+// const name = ["x@s@n @bdur@z@@@@@@@@@@@kov"].join().split("").filter((value) => value === "@" || value=='s');
+// console.log(name);
+
+// const meva =['olma','nok','behi']
+
+// const funMeva =(str) => {
+//        return meva.filter((value) => value !== str)
+// }
+
+// console.log(funMeva('olma'));
+
+// const  arr = [1,2,3,5,6,7].map()
+
+console.log(arr);
+disturakcha
+const  name = ['olma' ,'nok','behi','anor']
+
+const [muydinhon,n,o,l]  = name
+
+// spretor operater
+console.log(...name,'malumot','adsa');
+
+const compny = {
+  frontDev: [
+    {
+      name: "muydinhon",
+      age: 18,
+      id: 1,
+      price: 400,
+      status: "actice coder",
+    },
+    {
+      name: "muydinhon",
+      age: 18,
+      id: 2,
+      price: 500,
+      status: "actice coder",
+    },
+    {
+      name: "muydinhon",
+      age: 18,
+      id: 3,
+      price: 600,
+      status: "actice coder",
+    },
+    {
+      name: "muydinhon",
+      age: 18,
+      id: 4,
+      price: 700,
+      status: "actice coder",
+    },
+  ],
+  bakendDev: [
+    {
+      name: "muydinhon",
+      age: 18,
+      id: 1,
+      price: 400,
+      status: "actice coder",
+    },
+    {
+      name: "muydinhon",
+      age: 18,
+      id: 2,
+      price: 500,
+      status: "actice coder",
+    },
+    {
+      name: "muydinhon",
+      age: 18,
+      id: 3,
+      price: 600,
+      status: "actice coder",
+    },
+    {
+      name: "muydinhon",
+      age: 18,
+      id: 4,
+      price: 700,
+      status: "actice coder",
+    },
+  ],
+};
+
+console.log(compny.frontDev);
+
+const data = [
+  {
+    name: "muydinkhon",
+    id: 1,
+    hobbe: "suzuvchi",
+  },
+  {
+    name: "xusan",
+    id: 2,
+    hobbe: "uchuvchi",
+  },
+  {
+    name: "zubayir",
+    id: 3,
+    hobbe: "kamonchi",
+  },
+  {
+    name: "akmalhon",
+    id: 4,
+    hobbe: "gamer",
+  },
+];
+
+const deleteFun =(id) => {
+   return data.filter((value) => value.id !== id)
+}
+console.log(deleteFun(1));
+
+const searchFun = (str) => {
+  return     data.filter((value)=> value.name.includes(str) || value.hobbe.includes(str))
+};
+
+console.log(searchFun("n"));;
+
+Object.seal(object)
+object.name='muydinhon'
+object.add='xasan'
+
+Object.freeze(object)
+object.age=16;
+delete(object.name);
+console.log(object);
+
+res= object.map((value)=> {
+    // return Object.keys(value)
+    // return Object.values(value)
+    return Object.entries(value)
+
+})
+console.log(res);
+
+const SelectFun = (id) => {
+    return object.filter((value) => value.id == id)
 
 }
 
-searchFun()
+console.log(SelectFun(1));
